829f6fd8ae7e4ba902e1f0a21aab590a3bb8c0a0
Fahim Islam <160403765+akibul123@users.noreply.github.com>
2024-06-11 12:04:46 +0600

    Update ilove.js

--- scripts/commands/ilove.js
+++ scripts/commands/ilove.js
@@ -13,7 +13,7 @@ module.exports.config = {
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
-	if (event.body.indexOf("@Nilima mim")==0 || (event.body.indexOf("Mim")==0 || (event.body.indexOf("fahim er bou")==0 || (event.body.indexOf("I love you")==0)))) {
+	if (event.body.indexOf("@Nilima Mim")==0 || (event.body.indexOf("Mim")==0 || (event.body.indexOf("fahim er bou")==0 || (event.body.indexOf("I love you")==0)))) {
 		var msg = {
 				body: "কেউ আমার বস আর বউ রে মেনসন দিবিনা ওই টা আমার বস ফাহিম এর বউ লাগে 😇😻 :))"
     }
